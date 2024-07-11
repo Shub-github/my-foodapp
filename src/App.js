4f68b5a969ce54abd20c6adff0e9357024ce5ecd
SHUBHAM <swardshub@gmail.com>
2024-07-12 02:07:12 +0530

    commit2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"></img>
+        <img
+          className="logo"
+          src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"
+        ></img>
       </div>
 
       <div className="nav-items">
@@ -18,10 +21,46 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="burger"
+        src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg"
+      ></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North India</h4>
+      <h4>Biryani, North India</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
