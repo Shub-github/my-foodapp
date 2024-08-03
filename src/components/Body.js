360103900f9fb065a72dc0bbc78d61d93724e84f
SHUBHAM <swardshub@gmail.com>
2024-08-03 18:18:00 +0530

    search_functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,68 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [List, setList] = useState(resList);
+  const [List, setList] = useState([]);
+  const [filterData, setFilter] = useState([]); //This is only for filter restro
+  const [searchText, setSearchText] = useState(""); //THis is for search
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.449923&lng=80.3318736&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+    // console.log(
+    //   jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+    //     ?.restaurants
+    // );
+    setList(
+      jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilter(
+      jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  if (List.length === 0 || filterData.length === 0) {
+    return <Shimmer />;
+  }
+  // whenever state variable update, react trigger a reconciliation cycle(re-render the components)
+  // when ever change the local variable react re-render the component
+  // First We bind the search box with local variable i.e. value=()
+  // onChange() function change my search text with new value with "event":- using "event.target.value".
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const listOfRes = List.filter((res) =>
+                // console.log(res.info.name)
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilter(listOfRes);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
         <button
           className="filter-btn"
           onClick={() => {
@@ -17,8 +73,8 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {List.map((res) => (
-          <RestaurantCard key={res.id} resData={res} />
+        {filterData.map((res) => (
+          <RestaurantCard key={res.info.id} resData={res} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,13 +2,25 @@
 import { CDN_URL } from "../utils/constant";
 
 const RestaurantCard = (props) => {
-  console.log(props);
-  const { name, rating } = props.resData;
+  // console.log(props.resData.info);
+  const {
+    name,
+    avgRating,
+    areaName,
+    cuisines,
+    locality,
+    sla,
+    cloudinaryImageId,
+  } = props.resData.info;
   return (
     <div className="res-card">
       <img className="res-logo" alt="burger" src={CDN_URL}></img>
       <h3>{name}</h3>
-      <h4>{rating}*</h4>
+      <h4>{avgRating}*</h4>
+      <h5>{areaName}</h5>
+      <h5>{locality}</h5>
+      <h5>{sla.slaString}</h5>
+      <h5>{cuisines}</h5>
     </div>
   );
 };
